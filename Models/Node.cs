f2e5ed6147c694904af2869b86d3fa411e624d8d
x893 <x893.00@gmail.com>
2024-08-09 02:38:05 +0300

    Add README.md

--- Models/Node.cs
+++ Models/Node.cs
@@ -1,9 +1,13 @@
-﻿namespace RestApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestApi.Models
 {
 	public class Node
 	{
+		[Key]
 		public int Id { get; set; }
 		public int? ParentId { get; set; }
+		[MaxLength(50)]
 		public string? Name { get; set; }
 	}
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+# REST API TEST
+
+## Description
+
+This repository is Rest API test
+
+## Usage
+
+```html
+$ git clone https://github.com/x893/RestApiTest
+
+$ dotnet restore
+
+$ dotnet run
+
+```
